771723d83d9f1e54cc4c584cadff871af8b8dfa1
Maikel Arcia Carrazana <macarci@gmail.com>
2018-09-17 21:21:39 -0300

    Fixing simple type restrictions

--- lib/xsd/simple_type_restriction.rb
+++ lib/xsd/simple_type_restriction.rb
@@ -22,6 +22,11 @@ module Xsd
           @restrictions[name] ||= []
           @restrictions[name] << attributes
         else
+          if name == 'pattern'
+            if (_pattern = @restrictions[name])
+              attributes[0][1] = "(#{_pattern[0][1]})|(#{attributes[0][1]})"
+            end
+          end
           @restrictions[name] = attributes
         end
       end
@@ -29,12 +34,27 @@ module Xsd
     end
 
     def to_json_schema
-      json = documenting(qualify_type(base).to_json_schema)
+      json = documenting(qualify_type_ref(base).to_json_schema)
       @restrictions.each do |key, value|
         restriction =
           case key
           when 'enumeration'
-            { 'enum' => value.collect { |v| v[0][1] }.uniq }
+            enum = value.collect { |v| v[0][1] }.uniq
+            type_cast =
+              case json['type']
+              when 'integer'
+                :to_i
+              when 'number'
+                :to_f
+              when 'boolean'
+                :to_b
+              when 'string'
+                :to_s
+              else
+                nil # TODO Create a warning for enum type casting
+              end
+            enum = enum.map { |value| value && value.to_s.send(type_cast) } if type_cast
+            { 'enum' => enum }
           when 'length'
             { 'minLength' => value[0][1].to_i, 'maxLength' => value[0][1].to_i }
           when 'pattern'
@@ -47,10 +67,10 @@ module Xsd
             { 'minimum' => value[0][1].to_i, 'exclusiveMinimum' => true }
           when 'maxExclusive'
             { 'maximum' => value[0][1].to_i, 'exclusiveMaximum' => true }
-          when 'fractionDigits'
-            { 'multipleOf' => 1.0/(10 ** value[0][1].to_i) }
-          when 'totalDigits' #TODO Fractions digits count in total
-            { 'minimum' => 10 ** (value[0][1].to_i - 1), 'maximum' => 10 ** value[0][1].to_i - 1 }
+          # when 'fractionDigits' # TODO Include totalDigits fractionDigits in the validator
+          #   { 'multipleOf' => 1.0 / (10 ** value[0][1].to_i) }
+          # when 'totalDigits'
+          #   { 'maximum' => 10 ** value[0][1].to_i - 1 }
           else
             #TODO simpleType and whiteScpace restrictions
             { (key = key.gsub('xs:', '')) => value[0][1].to_i } unless value.empty?
